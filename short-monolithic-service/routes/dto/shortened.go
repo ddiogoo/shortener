6ddbebde290e0333d650422c2c5b178733679670
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-09-29 12:38:33 -0300

    refactor: Create "constructor" to request and response

--- short-monolithic-service/routes/dto/shortened.go
+++ short-monolithic-service/routes/dto/shortened.go
@@ -3,3 +3,21 @@ package dto
 type ShortenedRequest struct {
 	Url string `json:"url"`
 }
+
+func NewShortenedRequest() ShortenedRequest {
+	return ShortenedRequest{
+		Url: "",
+	}
+}
+
+type ShortenedResponse struct {
+	Id           uint  `json:"id"`
+	RowsAffected int64 `json:"rowsAffected"`
+}
+
+func NewShortenedResponse(id uint, rowsAffected int64) ShortenedResponse {
+	return ShortenedResponse{
+		Id:           id,
+		RowsAffected: rowsAffected,
+	}
+}
